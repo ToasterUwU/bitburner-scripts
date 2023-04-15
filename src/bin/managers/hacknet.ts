f01b11c1f7e4ac6dbf8dcc7d8e2ef4b9f3db8c42
ToasterUwU <aki@toasteruwu.com>
2023-04-16 00:31:51 +0200

    improved hacknet buying process

--- src/bin/managers/hacknet.ts
+++ src/bin/managers/hacknet.ts
@@ -3,38 +3,52 @@ import { TermLogger } from "/lib/helpers";
 
 /** @param {NS} ns **/
 export async function main(ns: NS): Promise<void> {
-
     const BUY_AMOUNT = (ns.args.length > 0 && typeof ns.args[0] == "number") ? ns.args[0] : 1 // if there is at least one arg and the first arg is a number, use that number, else use 1
     const KEEP_MONEY_MULTIPLIER = (ns.args.length > 1 && typeof ns.args[1] == "number") ? ns.args[1] : 5
     const LOGGER = new TermLogger(ns)
     LOGGER.success("Started Hacknet Manager")
 
+    const LEVEL_BATCH_SIZE = 50
+    const RAM_BATCH_SIZE = 16
+    const CORE_BATCH_SIZE = 2
+
     while (true) {
-        const cheapest: [number, () => boolean, string] = [Infinity, () => { return false }, ""] // price, buy function that returns boolean, name of the upgrade measure
-
-        if (ns.hacknet.getPurchaseNodeCost() < cheapest[0]) {
-            cheapest[0] = ns.hacknet.getPurchaseNodeCost()
-            cheapest[1] = () => { if (ns.hacknet.purchaseNode() >= 0) { return true } else { return false } }
-            cheapest[2] = "new Node"
-        }
-
-        for (let i = 0; i < ns.hacknet.numNodes(); i++) {
-            // super compact way of putting the cost getters, the buyers, and the names for what they buy together. Spares me from repeative code below this
-            const functions: [...[(i: number, n: number) => number, (i: number, n: number) => boolean, string][]] = [[ns.hacknet.getLevelUpgradeCost, ns.hacknet.upgradeLevel, "Level Upgrade"], [ns.hacknet.getRamUpgradeCost, ns.hacknet.upgradeRam, "RAM Upgrade"], [ns.hacknet.getCoreUpgradeCost, ns.hacknet.upgradeCore, "Core Upgrade"]]
-
-            for (const subset of functions) {
-                const price = subset[0](i, BUY_AMOUNT)
-                if (price < cheapest[0]) {
-                    cheapest[0] = price
-                    cheapest[1] = () => { return subset[1](i, BUY_AMOUNT) }
-                    cheapest[2] = subset[2]
-                }
+        if (ns.hacknet.numNodes() == 0) {
+            if (ns.hacknet.purchaseNode() >= 0) {
+                LOGGER.successToast("Bought first Hacknet Node")
             }
         }
 
-        if (cheapest[0] * KEEP_MONEY_MULTIPLIER <= ns.getPlayer().money || ns.hacknet.numNodes() == 0) {
-            if (cheapest[1]()) {
-                LOGGER.successToast("Bought:", BUY_AMOUNT.toString(), "Hacknet", cheapest[2])
+        for (let i = 0; i < ns.hacknet.numNodes(); i++) {
+            const stats = ns.hacknet.getNodeStats(i)
+
+            for (let j = 1; j <= 4; j++) {
+                if (stats.level < LEVEL_BATCH_SIZE * j) {
+                    if (ns.hacknet.getLevelUpgradeCost(i, BUY_AMOUNT) * KEEP_MONEY_MULTIPLIER <= ns.getPlayer().money) {
+                        if (ns.hacknet.upgradeLevel(i, BUY_AMOUNT)) {
+                            LOGGER.info("Bought Hacknet level upgrade")
+                        }
+                    }
+                    break
+                } else if (stats.ram < RAM_BATCH_SIZE * j) {
+                    if (ns.hacknet.getRamUpgradeCost(i, BUY_AMOUNT) * KEEP_MONEY_MULTIPLIER <= ns.getPlayer().money) {
+                        if (ns.hacknet.upgradeRam(i, BUY_AMOUNT)) {
+                            LOGGER.info("Bought Hacknet RAM upgrade")
+                        }
+                    }
+                    break
+                } else if (stats.cores < CORE_BATCH_SIZE * j) {
+                    if (ns.hacknet.getCoreUpgradeCost(i, BUY_AMOUNT) * KEEP_MONEY_MULTIPLIER <= ns.getPlayer().money) {
+                        if (ns.hacknet.upgradeCore(i, BUY_AMOUNT)) {
+                            LOGGER.info("Bought Hacknet Core upgrade")
+                        }
+                    }
+                    break
+                } else {
+                    if (ns.hacknet.purchaseNode() >= 0) {
+                        LOGGER.successToast("Bought new Hacknet Node")
+                    }
+                }
             }
         }
 
