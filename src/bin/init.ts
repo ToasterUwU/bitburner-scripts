b6564ab00284ae36f94c2c9e1960955b1d1083f2
ToasterUwU <aki@toasteruwu.com>
2023-04-19 04:09:25 +0200

    wait until enough ram, line for later commented

--- src/bin/init.ts
+++ src/bin/init.ts
@@ -1,7 +1,7 @@
 import { NS } from "@ns"
 import { TermLogger } from "/lib/helpers"
 
-const MANAGER_PRIOS: Record<string, number> = { "watcher.js": 1, "hacknet.js": 0, "hacking.js": 2 }
+const MANAGER_PRIOS: Record<string, number> = { "hacknet.js": 0, "watcher.js": 1, "hacking.js": 2 }
 
 function compareManagerPrios(manager1: string, manager2: string) {
     const NAME1 = manager1.split("/")[-1]
@@ -22,13 +22,15 @@ export async function main(ns: NS): Promise<void> {
 
     for (const manager of ns.ls("home", "/bin/managers").sort(compareManagerPrios).reverse()) {
         if (ns.getScriptRam(manager) > ns.getServerMaxRam("home") - ns.getServerUsedRam("home")) { // If not enough free RAM currently
-            if ((ns.getServerMaxRam("home") - ns.getServerUsedRam("home")) + ns.getScriptRam("/bin/init.js") < ns.getScriptRam(manager)) { // If not enough RAM even when killing this script
-                LOGGER.err("Cannot start", manager, "because of missing RAM")
-            } else { // If enough RAM when killing init.js
-                LOGGER.info("Starting", manager, "but need to kill current script first for freeing RAM")
-                await ns.sleep(1000)
-                ns.spawn(manager)
+            while ((ns.getServerMaxRam("home") - ns.getServerUsedRam("home")) + ns.getScriptRam("/bin/init.js") < ns.getScriptRam(manager)) { // If not enough RAM even when killing this script
+                // ns.singularity.upgradeHomeRam()
+                await ns.sleep(500)
             }
+
+            // If enough RAM when killing init.js
+            LOGGER.info("Starting", manager, "but need to kill current script first for freeing RAM")
+            await ns.sleep(1000)
+            ns.spawn(manager)
 
         } else { // If enough RAM
             const pid = ns.run(manager)
