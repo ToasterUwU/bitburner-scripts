3a1e13ae02e3e5d82f30ca15953c1ab368a5efd0
ToasterUwU <aki@toasteruwu.com>
2023-04-19 02:06:52 +0200

    seperated functiions

--- src/bin/utils/customScan.ts
+++ src/bin/utils/customScan.ts
@@ -1,36 +1,37 @@
 import { NS } from "@ns";
 import { Navigation, RecursiveDictionary } from "/lib/helpers";
 
+
+const GREEN = "\u001b[32m";
+const RED = "\u001b[31m";
+const RESET = "\u001b[0m";
+
+function recursivePrint(ns: NS, computerMap: RecursiveDictionary, depth = 0) {
+    for (const host in computerMap) {
+        let text = ""
+
+        for (let i = 0; i < depth; i++) {
+            text += "\t"
+        }
+
+        if (ns.hasRootAccess(host)) {
+            text += GREEN
+        } else {
+            text += RED
+        }
+        text += host + RESET
+
+        ns.tprint(text)
+
+        recursivePrint(ns, computerMap[host], depth + 1)
+    }
+}
+
 /** @param {NS} ns **/
 export async function main(ns: NS): Promise<void> {
-    const green = "\u001b[32m";
-    const red = "\u001b[31m";
-    const reset = "\u001b[0m";
-
     const COMPUTER_MAP = Navigation.recursiveScan(ns, ns.getHostname(), true)
 
     ns.print(COMPUTER_MAP)
 
-    function recursivePrint(computerMap: RecursiveDictionary, depth = 0) {
-        for (const host in computerMap) {
-            let text = ""
-
-            for (let i = 0; i < depth; i++) {
-                text += "\t"
-            }
-
-            if (ns.hasRootAccess(host)) {
-                text += green
-            } else {
-                text += red
-            }
-            text += host + reset
-
-            ns.tprint(text)
-
-            recursivePrint(computerMap[host], depth + 1)
-        }
-    }
-
-    recursivePrint(COMPUTER_MAP)
+    recursivePrint(ns, COMPUTER_MAP)
 }
