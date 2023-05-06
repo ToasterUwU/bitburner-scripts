8556bede2c716b98407ad316050698a6c5776ed0
ToasterUwU <aki@toasteruwu.com>
2023-05-06 13:02:39 +0200

    feat(helpers): ChatGPT made NodeStats comparing function

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,7 +1,48 @@
-import { NS } from "@ns"
+import { NS, NodeStats } from "@ns"
 
 export interface RecursiveDictionary {
     [Key: string]: RecursiveDictionary;
+}
+
+export const Hacknet = {
+    /**
+     * Compare two NodeStats objects based on their properties.
+     * Returns a negative number if nodeA is "smaller" than nodeB,
+     * a positive number if nodeA is "larger" than nodeB,
+     * and 0 if they are equal.
+     * @param {NodeStats} nodeA - The first NodeStats object to compare.
+     * @param {NodeStats} nodeB - The second NodeStats object to compare.
+     * @returns {number} The comparison result.
+     */
+    compareNodeStats(nodeA: NodeStats, nodeB: NodeStats): number {
+        if (nodeA.level !== nodeB.level) {
+            return nodeA.level - nodeB.level;
+        }
+
+        if (nodeA.ram !== nodeB.ram) {
+            return nodeA.ram - nodeB.ram;
+        }
+
+        if (nodeA.cores !== nodeB.cores) {
+            return nodeA.cores - nodeB.cores;
+        }
+
+        // Check optional properties if they exist
+        if (nodeA.ramUsed && nodeB.ramUsed && nodeA.ramUsed !== nodeB.ramUsed) {
+            return nodeA.ramUsed - nodeB.ramUsed;
+        }
+
+        if (nodeA.cache && nodeB.cache && nodeA.cache !== nodeB.cache) {
+            return nodeA.cache - nodeB.cache;
+        }
+
+        if (nodeA.hashCapacity && nodeB.hashCapacity && nodeA.hashCapacity !== nodeB.hashCapacity) {
+            return nodeA.hashCapacity - nodeB.hashCapacity;
+        }
+
+        // If all properties are the same, return 0
+        return 0;
+    }
 }
 
 export const ReadText = {
